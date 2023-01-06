0472714ba46df73c9657c92325efaafc3a089e97
SamjoeTL <tegar.terbaik@gmail.com>
2023-01-06 21:46:30 +0800

    hanya menghapus models dan controller di route

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
-use App\Models\Student;
+
 
 /*
 |--------------------------------------------------------------------------
